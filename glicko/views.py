17ffa7003110707bfb1b923bb467366e21980af6
bliutwo <bliutwo@cs.stanford.edu>
2019-08-28 22:43:59 +0900

    Add a view for first form glicko

--- glicko/views.py
+++ glicko/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the glicko index.")
+    context = {}
+    return render(request, 'glicko/index.html', context)
+
